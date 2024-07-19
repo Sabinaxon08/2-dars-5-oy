dc6109bad824c01e237e2587ff94e50eb61f01ca
Sabinaxon08 <sabdugaffarovasabina@gmaSabinaxon08il.com>
2024-07-19 15:06:19 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+// object oriented programming -OOP
+
+// let account = {
+//     profileimage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVHJPKwMrfYLW6UdsHPbEo-cJi9HcFMODqRw&s",
+//     username: "Sabina Abdugaffarova",
+//     nickname:"sabina_A",
+//     followers:"14",
+//     following: "20",
+//     bio: "I will succeed",
+//     post: [
+//         {
+//             id:1,
+//             postImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVHJPKwMrfYLW6UdsHPbEo-cJi9HcFMODqRw&s",
+//             content:"lorem ipsum ",
+//             likes:0,
+//             comments:0,
+//         }
+//     ]
+        
+    
+// }
+
+// console.log(account.post.[0].postimage)
+
+
+// let nickname = document.querySelector("nickname")
+// let profileimage = document.querySelector("") 
+
+
+
+
+let name = prompt("ism yozing");
+let spaceId = prompt("id kiriting");
+let spacePassword = prompt("password kiriting");
+
+
+let userData = {
+    name: name,
+    ID: spaceId,
+    password: spacePassword
+  };
+
+
+console.log(userData);
